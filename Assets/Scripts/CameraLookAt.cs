f80812ad72a943084d4f6a2c2e15c07e63b758a1
Oliver Ellmers <oliver@random42.com>
2018-08-17 16:14:13 +0100

    Additional Mouse Lookat Interactivity

---
+++ Assets/Scripts/CameraLookAt.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLookAt : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float midPoint = (transform.position - Camera.main.transform.position).magnitude * 0.5f;
+        transform.LookAt(mouseRay.origin + mouseRay.direction * midPoint);
+        //OrthoCamera
+        //transform.LookAt(Camera.main.ScreenToViewportPoint(Input.mousePosition));
+	}
+}
