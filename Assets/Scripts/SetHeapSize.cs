130cc1d4a13672471f58ee43c10f557b826a0643
Oliver Ellmers <oliver@random42.com>
2018-08-17 17:51:14 +0100

    SmallerMemory

---
+++ Assets/Scripts/SetHeapSize.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class SetHeapSize : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        PlayerSettings.WebGL.emscriptenArgs = "-s WASM_MEM_MAX=512MB";
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
